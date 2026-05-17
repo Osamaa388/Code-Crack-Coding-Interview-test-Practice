4ecb5bd9aa7b9bcdf59e99edc2962c5c449385c0
Osamaa388 <164921209+Osamaa388@users.noreply.github.com>
2026-05-18 04:21:03 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,8 @@ app.use(cors({
       return callback(null, true);
     }
 
-    return callback(new Error("Not allowed by CORS"));
+    // IMPORTANT: DO NOT THROW ERROR
+    return callback(null, false);
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
